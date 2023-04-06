24915662e1782efcc728ae7b7bb4ab933d44f3a0
Shrey Marwaha <shrey19334@iiitd.ac.in>
2023-04-06 21:07:50 +0530

    /fix whatsapp share

--- extension/src/Components/Share.js
+++ extension/src/Components/Share.js
@@ -2,18 +2,13 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import { MdWhatsapp } from "react-icons/md";
 import { FacebookShareButton, FacebookIcon, WhatsappShareButton, WhatsappIcon } from "react-share";
-function Share(url) {
-	console.log("url", url);
-	// function to url encode the text
-	const urlEncode = (text) => {
-		return encodeURIComponent(text);
-	};
+function Share(props) {
 	const openInNewTab = (url) => {
 		window.open(url, "_blank", "noopener,noreferrer");
 	};
 	return (
 		<div className='mb-2 mx-3'>
-			<Button variant='outline-success' onClick={() => openInNewTab(`https://web.whatsapp.com/send/?text=${urlEncode(url)}`)}>
+			<Button variant='outline-success' onClick={() => openInNewTab(`https://web.whatsapp.com/send/?text=${encodeURIComponent(props.url)}`)}>
 				<MdWhatsapp className='me-2' style={{ width: "25px", height: "25px" }} />
 				Share on Whatsapp
 			</Button>
