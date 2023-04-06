11adf0eedda92dff8d73bc362911f9a991f4a9c2
Shrey Marwaha <shrey19334@iiitd.ac.in>
2023-04-06 21:13:21 +0530

    removed unused code

--- extension/src/Pages/Settings.js
+++ extension/src/Pages/Settings.js
@@ -51,31 +51,3 @@ function Settings() {
 }
 
 export default Settings;
-
-// <Tabs defaultActiveKey='General' id='uncontrolled-tab-example' className='mt-3'>
-// 	<Tab eventKey='General' title='General' className='m-3 bg-primary'>
-// 		<button>ancb</button>
-// 	</Tab>
-// 	<Tab eventKey='Keys' title='Manage Keys'>
-// 		<button>ancb</button>
-// 	</Tab>
-// 	<Tab eventKey='Import/Export Keys' title='Import/Export Keys'>
-// 		<Accordion defaultActiveKey='0' className='mx-3 mt-3'>
-// 			<Accordion.Item eventKey='0'>
-// 				<Accordion.Header>
-// 					<IoSettingsOutline className='me-2' />
-// 					General Settings
-// 				</Accordion.Header>
-// 				<Accordion.Body>
-// 					<p>default key generation length</p>
-// 				</Accordion.Body>
-// 			</Accordion.Item>
-// 			<Accordion.Item eventKey='1'>
-// 				<Accordion.Header>
-// 					<MdVpnKey className='me-2' /> Key Management
-// 				</Accordion.Header>
-// 				<Accordion.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Accordion.Body>
-// 			</Accordion.Item>
-// 		</Accordion>
-// 	</Tab>
-// </Tabs>
