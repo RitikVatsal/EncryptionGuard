9b68205c2c4bc4db01ee391b4d322ef75e690626
Shrey Marwaha <shrey19334@iiitd.ac.in>
2023-04-06 07:35:14 +0530

    back button

--- extension/src/Pages/Settings.js
+++ extension/src/Pages/Settings.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { IoSettingsOutline } from "react-icons/io5";
 import { VscKey } from "react-icons/vsc";
 import { RiFolderTransferLine } from "react-icons/ri";
+import { MdChevronLeft, MdClose } from "react-icons/md";
 // Bootstrap components
 
 // Custom Components
@@ -11,21 +12,30 @@ import GeneralSettings from "./GeneralSettings";
 import KeyManagement from "./KeyManagement";
 import ImportExportKeys from "./ImportExportKeys";
 
+import "../Components/Settings.css";
+
 function Settings() {
-	const [settingsPage, setSettingsPage] = useState("Settings");
+	const [settingsPage, setSettingsPage] = useState(0);
+
 	return (
 		<>
-			{settingsPage === "Settings" ? (
-				<div className='m-3 mt-5'>
-					<SettingsNav icon={<IoSettingsOutline style={{ width: "20px", height: "20px" }} />} title='General' onClick={() => setSettingsPage("General")} />
-					<SettingsNav icon={<VscKey style={{ width: "20px", height: "20px" }} />} title='Key Management' onClick={() => setSettingsPage("Key Management")} />
-					<SettingsNav icon={<RiFolderTransferLine style={{ width: "20px", height: "20px" }} />} title='Keys Import/Export' onClick={() => setSettingsPage("Keys Import/Export")} />
+			{settingsPage > 9 && (
+				<div className='d-flex align-items-center my-4 mx-2 btn w-25 back-btn' style={{ width: "auto", backgroundColor: "transparent" }} onClick={() => setSettingsPage(0)}>
+					<MdChevronLeft style={{ width: "35px", height: "35px" }} />
+					<h5 className='mb-0 ml-2'>Back</h5>
 				</div>
-			) : settingsPage === "General" ? (
+			)}
+			{settingsPage < 10 ? (
+				<div className='m-3 mt-5'>
+					<SettingsNav icon={<IoSettingsOutline style={{ width: "20px", height: "20px" }} />} title='General' onClick={() => setSettingsPage(11)} />
+					<SettingsNav icon={<VscKey style={{ width: "20px", height: "20px" }} />} title='Key Management' onClick={() => setSettingsPage(12)} />
+					<SettingsNav icon={<RiFolderTransferLine style={{ width: "20px", height: "20px" }} />} title='Keys Import/Export' onClick={() => setSettingsPage(13)} />
+				</div>
+			) : settingsPage === 11 ? (
 				<GeneralSettings />
-			) : settingsPage === "Key Management" ? (
+			) : settingsPage === 12 ? (
 				<KeyManagement />
-			) : settingsPage === "Keys Import/Export" ? (
+			) : settingsPage === 13 ? (
 				<ImportExportKeys />
 			) : null}
 		</>
